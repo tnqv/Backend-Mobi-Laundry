641a27a018fa58a0263a175f556cef17305a0b68
tnqv102 <tnqv1021997@gmail.com>
2018-11-10 22:49:17 +0700

    Update docker compose

--- models/review.go
+++ models/review.go
@@ -2,14 +2,13 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	"d2d-backend/accounts"
 )
 
 type Review struct {
 	gorm.Model						`json:"-"`
 	Content 	   string			`json:"content"`
 	Rate           int		   		`json:"rate"`
-	UserRate       accounts.User  	`json:"user"`
+	UserRate       User  	`json:"user"`
 	UserID		   int				`json:"-"`
 	User		   User 			`json:"user" gorm:"save_associations:false"`
 }
