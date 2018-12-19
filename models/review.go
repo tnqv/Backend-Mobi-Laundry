271f73ddabd763be24c2e9c02644abef81e5d881
tnqv102 <tnqv1021997@gmail.com>
2018-12-20 00:30:28 +0700

    fix review model

--- models/review.go
+++ models/review.go
@@ -8,7 +8,7 @@ type Review struct {
 	gorm.Model
 	Content 	   string			`json:"content"`
 	Rate           int		   		`json:"rate"`
-	UserRate       User  			`json:"user" gorm:"save_associations:false"`
-	UserID		   int				`json:"user_id"`
+	//UserRate       User  			`json:"user" gorm:"save_associations:false"`
+	UserID		   uint				`json:"user_id"`
 	User		   User 			`json:"user" gorm:"save_associations:false"`
 }
